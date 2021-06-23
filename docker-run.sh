fac78bc9e2f5ed0bd44ff5a186d17c7e2a95c218
setyodwi <setyoblater25@gmail.com>
2021-06-23 16:39:54 +0700

    ganti port publish

--- docker-run.sh
+++ docker-run.sh
@@ -3,5 +3,5 @@
 docker stop cas > /dev/null 2>&1
 docker rm cas > /dev/null 2>&1
 image_tag=(`cat gradle.properties | grep "cas.version" | cut -d= -f2`)
-docker run -d -p 8282:8080 -p 10443:8443 --name="cas" apereo/cas:"v${image_tag}"
+docker run -d -p 8282:80 -p 10443:443 --name="cas" apereo/cas:"v${image_tag}"
 docker logs -f cas
